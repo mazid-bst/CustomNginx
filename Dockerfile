cb37ad3ea39bdda994c1db30f88254a4d8baaa4f
mazid-bst <mohammad.mazid@bluestacks.com>
2022-06-01 13:40:38 +0530

    .

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,9 @@
 FROM nginx
-ADD ./index.html /usr/share/nginx/html/
+
+RUN apk update
+RUN apk add --no-cache --virtual .build-deps g++ python3-dev libffi-dev openssl-dev && \
+    pip3 install --upgrade pip setuptools && \
+    apk del .build-deps
+RUN apk add --no-cache --update python3
+ADD ./index.py  /usr/share/nginx/html/
+RUN /usr/bin/python3 /usr/share/nginx/html/index.py
---
+++ index.py
@@ -0,0 +1,23 @@
+import os
+# to open/create a new html file in the write mode
+f = open('/usr/share/nginx/html/index.html', 'w')
+
+# the html code which will go in the file GFG.html
+html_template = """<html>
+<head>
+<title>Welcome to Bluestacks </title>
+</head>
+<body>
+<h2>FROM """+os.getenv['REGION']+"""</h2>
+
+<p>Default code has been loaded into the Editor.</p>
+
+</body>
+</html>
+"""
+
+# writing the code into the file
+f.write(html_template)
+
+# close the file
+f.close()
